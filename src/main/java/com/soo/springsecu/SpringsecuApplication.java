5d16d5310cafd000764b59df24c05c23bdbaf021
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-07-20 16:34:03 +0000

    iniital commit avec les dependance nessecaires

---
+++ src/main/java/com/soo/springsecu/SpringsecuApplication.java
@@ -0,0 +1,13 @@
+package com.soo.springsecu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuApplication.class, args);
+    }
+
+}
